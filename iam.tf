d572621261b7cb88d59e650c0e550b2332c9f2da
Álvaro Braz <alvarobraz83@gmail.com>
2026-02-09 14:38:59 -0300

    feat: configure-roles

--- iam.tf
+++ iam.tf
@@ -13,3 +13,37 @@ resource "aws_iam_openid_connect_provider" "oidc-git" {
     IAC = "true"
   }
 }
+
+resource "aws_iam_role" "ecr-role" {
+  name = "ecr-role"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Principal" : {
+          "Federated" : "arn:aws:iam::530482047728:oidc-provider/token.actions.githubusercontent.com"
+        },
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : [
+              "sts.amazonaws.com"
+            ]
+          },
+          "StringLike" : {
+            "token.actions.githubusercontent.com:sub" : [
+              "repo:alvarobraz/devops-iac-api:ref:refs/heads/main",
+              "repo:alvarobraz/devops-iac-api:ref:refs/heads/main"
+            ]
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    IAC = "true"
+  }
+}
